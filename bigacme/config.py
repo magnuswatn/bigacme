4ff867d51ebae0c39069169fe16348b8e1a4cfc3
Magnus Watn <magnus@watn.no>
2018-01-28 15:55:07 +0100

    Removed unused import

--- bigacme/config.py
+++ bigacme/config.py
@@ -6,7 +6,6 @@ from collections import namedtuple
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-from acme import jose
 
 logger = logging.getLogger(__name__)
 
